9aa0640830208b1d4cdd370fbe76d32f82618404
Thyago Chrystiann <thyago.chrystiann@hotmail.com>
2023-08-11 11:13:18 -0300

    Adicionando a função para dividir.

--- scm.c
+++ scm.c
@@ -8,9 +8,18 @@ void multiplica(int a, int b) {
     printf("%d x %d = %d\n", a, b, a * b);
 }
 
+void divide(int a, int b) {
+    if (b == 0) {
+        printf("Impossível dividir por 0!");
+    } else {
+        printf("%d / %d = %.2f\n", a, b, a / (float)b);
+    }
+}
+
 int main() {
     printf("Olá, Estudante!\n");
     soma(5, 3);
     multiplica(5, 3);
+    divide(5, 3);
     return 0;
 }
