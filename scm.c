afe29628347d7835a74d16cf463af467ef0a53f6
Paulo <p.dantas@outlook.com>
2023-08-11 10:54:23 -0300

    Implementação da data e hora na saudação

--- scm.c
+++ scm.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stlib.h>
+#include<time.h>
 
 void soma(int a, int b) {
     printf("%d + %d = %d\n", a, b, a + b);
@@ -8,7 +10,14 @@ void multiplica(int a, int b) {
     printf("%d x %d = %d\n", a, b, a * b);
 }
 
+void data_hora(){
+ time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    printf("Agora: %d-%02d-%02d %02d:%02d:%02d\n\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour -3, tm.tm_min, tm.tm_sec);
+}
+
 int main() {
+    data_hora();
     printf("Olá, Estudante!\n");
     soma(5, 3);
     multiplica(5, 3);
